83aacc69f4f52afc3f7dfd13fe3138d2502b5bb4
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-03 14:41:24 +0700

    add version

--- dist/js/k-responsive-menu.js
+++ dist/js/k-responsive-menu.js
@@ -42,13 +42,15 @@
                 $pw,
                 $pp,
                 $sl,
+                $bd,
                 getAttr = this.element.attr('k-responsive-menu');
             if (getAttr && getAttr.length > 0) {
                 // Khai báo bởi Attr
                 $e = this.element, $o = $e.attr('k-menu-resize') ? isNaN($e.attr('k-menu-resize')) ? $e.attr('k-menu-resize') : parseInt($e.attr('k-menu-resize')) : this.options.resizeWidth, $d = $e.attr('k-menu-type') ? $e.attr('k-menu-type').toLocaleLowerCase() : this.options.menuType, $i = $e.attr('k-menu-icon') ? $e.attr('k-menu-icon').toLocaleLowerCase() : this.options.menuIcon, $p = $e.attr('k-menu-push') ? $e.attr('k-menu-push').toLocaleLowerCase() : this.options.menuPush, $pp = $e.attr('k-menu-position') ? $e.attr('k-menu-position').toLocaleLowerCase() : this.options.menuPushPosition, $pw = $e.attr('k-menu-width') ? $e.attr('k-menu-width').toLocaleLowerCase() : this.options.menuPushWidth, $sl = $e.attr('k-menu-speed') ? isNaN($e.attr('k-menu-speed')) ? $e.attr('k-menu-speed') : parseInt($e.attr('k-menu-speed')) : this.options.animationSpeed;
+                $bd = $e.attr('k-menu-backdrop') ? Boolean($e.attr('k-menu-backdrop')) : this.options.menuBackDrop;
             } else {
                 // Khai báo bởi JS
-                $e = this.element, $o = this.options.resizeWidth, $d = this.options.menuType, $i = this.options.menuIcon, $p = this.options.menuPush, $pp = this.options.menuPushPosition, $pw = this.options.menuPushWidth, $sl = this.options.animationSpeed;
+                $e = this.element, $o = this.options.resizeWidth, $d = this.options.menuType, $i = this.options.menuIcon, $p = this.options.menuPush, $pp = this.options.menuPushPosition, $pw = this.options.menuPushWidth, $sl = this.options.animationSpeed, $bd = this.options.menuBackDrop;
             }
             // Add Class Default
             $e.addClass('k-responsive-menu');
@@ -57,6 +59,11 @@
             // DO MENU 
             var nguyenApp = {
                 resizeTimer: null,
+                doBackDrop: function doBackDrop() {
+                    if (!$bd) {} else {
+                        $('body').append('<div class="k-menu-backdrop"></div>');
+                    }
+                },
                 doMenuPush: function doMenuPush() {
                     // Hiển thị
                     // Left   => return 1
@@ -96,42 +103,77 @@
                     }
                     $('.k-menu-toggle').on('click', function (event) {
                         event.preventDefault();
+                        var el = $(this);
                         var getFather = $(this).attr('k-toggle-for');
                         if (toggle == 1) {
                             $(this).removeClass('active');
-                            switchMenu(getFather);
+                            switchMenu(getFather, toggle, el);
                             toggle = 0;
                         } else {
                             $(this).addClass('active');
-                            switchMenu(getFather);
+                            switchMenu(getFather, toggle, el);
                             toggle = 1;
                         }
                     });
 
-                    var switchMenu = function switchMenu(getFather) {
+                    var switchMenu = function switchMenu(getFather, toggle, el) {
                         if (__type == 3) {
                             if ($p) {
+                                $('.k-menu-backdrop').addClass('active').bind('click', function () {
+                                    $(this).removeClass('active');
+                                    $(el).removeClass('active');
+                                    if ($p === 'right') {
+                                        var n = $(getFather).css("right");
+                                        $(getFather).animate({
+                                            right: "-" + $pw
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
+                                    } else {
+                                        var n = $(getFather).css("left");
+                                        $(getFather).animate({
+                                            left: "-" + $pw
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
+                                    }
+                                    $(this).unbind();
+                                });
                                 if ($p === 'right') {
                                     var n = $(getFather).css("right");
                                     if (n != '0px') {
                                         $(getFather).animate({
                                             right: "0"
-                                        }, __speed);
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
                                     } else {
                                         $(getFather).animate({
                                             right: "-" + $pw
-                                        }, __speed);
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
                                     }
                                 } else {
                                     var n = $(getFather).css("left");
                                     if (n != '0px') {
                                         $(getFather).animate({
                                             left: "0"
-                                        }, __speed);
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
                                     } else {
                                         $(getFather).animate({
                                             left: "-" + $pw
-                                        }, __speed);
+                                        }, {
+                                            duration: __speed,
+                                            complete: function complete() {}
+                                        });
                                     }
                                 }
                             } else {
@@ -199,6 +241,7 @@
             };
 
             nguyenApp.menuBar();
+            nguyenApp.doBackDrop();
             nguyenApp.doResponsiveMenu();
 
             plugin.element.on('click' + '.' + plugin._name, function () {
@@ -313,6 +356,7 @@
         menuPush: null, // right, left
         menuPushPosition: 'absolute', // fixed
         menuPushWidth: '100%', // px, %, rem
+        menuBackDrop: false,
         menuIcon: null,
         onResize: null,
         onComplete: null,
